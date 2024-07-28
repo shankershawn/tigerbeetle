4126eec078cce2d0c5ed3651a3a59f215de6f05b
Shankarsan Ganai <shankershawn@gmail.com>
2024-07-28 18:29:50 +0530

    First commit

---
+++ src/main/java/com/shankarsan/tigerbeetle/TigerbeetleApplication.java
@@ -0,0 +1,13 @@
+package com.shankarsan.tigerbeetle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerbeetleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerbeetleApplication.class, args);
+	}
+
+}
